097baab841b66eed9b98aad4376d32f2031c5ece
Sumsuzzaman Chowdhury <joysho@gmail.com>
2024-07-03 13:45:11 +0600

    All files added

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,8 @@
+<?php
+$celsius = 32; // Temperature in Celsius
+
+// Conversion formula from Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print the result with 2 decimal points
+printf("Temperature in Fahrenheit: %.2f", $fahrenheit);
